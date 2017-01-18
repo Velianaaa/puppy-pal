767e95dfbf94ad54a0127552e9e513009ccd8c5e
Slaven Ivanov <slaven.ivanov.95@gmail.com>
2017-01-19 00:43:26 +0200

    Test Commit 2

--- PuppyPal/ViewController.swift
+++ PuppyPal/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//Test COmmit
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
